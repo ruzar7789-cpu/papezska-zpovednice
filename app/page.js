829af1dea91aab8bb18ed20c5e95019c1e62c7be
ruzar7789-cpu <ruzar7789@gmail.com>
2026-05-23 18:17:38 +0200

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '50px', textAlign: 'center', fontFamily: 'serif' }}>
+      <h1>Digitální zpovědnice</h1>
+      <p>Vítejte v posvátném prostoru.</p>
+    </main>
+  );
+}
